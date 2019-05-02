59549d6a3368afe9ff1bc47560b0dcef7d5daff9
Magnus <magnuspetersonmunoz@gmail.com>
2019-05-02 03:53:30 +0000

    change

--- Javascript/calculator.js
+++ Javascript/calculator.js
@@ -1,4 +1,4 @@
 var age = prompt("How old are you?")
 // I don't know if it's a console.log message, or an alert message, so I'm doing both. 
-console.log("You are " + age * 365 + "days old")
-alert("You are" + age + "days old")
+console.log("You are " + age * 365.25 + " days old")
+alert("You are " + age * 365.25 + " days old")
