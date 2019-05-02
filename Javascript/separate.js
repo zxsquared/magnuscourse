7bc90b677bd96f6a77e28419d4c1bd5dfe6223df
Magnus <magnuspetersonmunoz@gmail.com>
2019-05-02 03:10:37 +0000

    changed stuff

--- Javascript/separate.js
+++ Javascript/separate.js
@@ -1,1 +1,7 @@
-alert ("Hi")
+var firstName = prompt("What is your first name");
+var sirName = prompt("What is your last name");
+
+var age = prompt("What is your age");
+console.log ("Your full name is " + firstName + " " + sirName);
+//To waste less space, instead of console.log ("Your full name is " + firstName + (" ") + sirName);, you can use console.log ("Your full name is " + firstName + " " + sirName); for a space between two variables.
+console.log ("you are " + age)
