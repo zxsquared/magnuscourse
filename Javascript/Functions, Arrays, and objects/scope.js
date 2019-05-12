a40e3591087c901b85c7f9cac7e67ac973f9e2b2
Magnus <magnuspetersonmunoz@gmail.com>
2019-05-12 23:51:45 +0000

    bvhg

--- Javascript/Functions, Arrays, and objects/scope.js
+++ Javascript/Functions, Arrays, and objects/scope.js
@@ -1,28 +1,20 @@
-// window.setTimeout(function() {
-// // Put JS Code here
-// }, 500)
 
-var todos = ["brbkr,gebj"]
+
+var todo = ["brbkr,gebj"]
 
 var input = prompt("What would you like to do?")
-
-
 while (input !== "quit") {
-
     if(input === "list"){
-        todos.forEach(function(todo){
+        todo.forEach(function(todo){
             console.log(todo);
-        });
-        
+        });    
     }
-    
     else if (input === "new") {
     var newTodo = prompt("Enter new todo")
     
-    todos.push(newTodo)
+    todo.push(newTodo)
     }
     // Ask again for input
     var input = prompt("What would you like to do?")
 }
-
 console.log("OK, you quit the app")
