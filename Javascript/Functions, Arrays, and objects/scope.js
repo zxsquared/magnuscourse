ab893cc3521c24d7cb7a9651fa372de99b5f8081
Magnus <magnuspetersonmunoz@gmail.com>
2019-05-13 23:13:09 +0000

    changed scope.js

--- Javascript/Functions, Arrays, and objects/scope.js
+++ Javascript/Functions, Arrays, and objects/scope.js
@@ -1,20 +1,45 @@
-
-
-var todo = ["brbkr,gebj"]
+// Original variables
+var todos = [""]
 
 var input = prompt("What would you like to do?")
-while (input !== "quit") {
-    if(input === "list"){
-        todo.forEach(function(todo){
-            console.log(todo);
-        });    
+    while (input !== "quit") {
+        if(input === "list"){
+      listTodos()
+        }
+        else if (input === "new") {
+       newTodo()
+        }
+        else if (input === "delete") {
+            deleteTodo()
+        }
+
+        // Ask again for input
+        var input = prompt("What would you like to do?")
     }
-    else if (input === "new") {
+console.log("OK, you quit the app")
+// Functions to shorten while loop
+
+                function listTodos() {
+    console.log("************")
+    todos.forEach(function(todo, i){
+
+        console.log(i + ": " + todo)
+    })    
+    console.log("************")
+                }
+
+                function newTodo() {
     var newTodo = prompt("Enter new todo")
     
-    todo.push(newTodo)
-    }
-    // Ask again for input
-    var input = prompt("What would you like to do?")
-}
-console.log("OK, you quit the app")
+    todos.push(newTodo)
+    console.log("Added todo")
+                }
+
+                function deleteTodo() {
+    // Ask dfor index of todo
+    var index = prompt("Enter index of todo you want to delete.")
+    // Delete todo
+    // Splice
+    todos.splice(index, 1)
+    console.log("Deleted todo")
+                }
