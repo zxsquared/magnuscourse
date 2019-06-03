bf7e59c4733dc1dbe7aab523ae50a566da1ec6e9
Magnus <magnuspetersonmunoz@gmail.com>
2019-06-03 16:51:14 -0600

    changed a lot

---
+++ backendCoding/express/firstExpress.js
@@ -0,0 +1,4 @@
+// Push documents to Github when I get home
+import express from "npm";
+
+var app = express();
---
+++ backendCoding/express/firstExpressApp/firstExpress.js
@@ -0,0 +1,4 @@
+// Push documents to Github when I get home
+var express1 = require("express");
+
+var app = express();
