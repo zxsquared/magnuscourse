d1ac57c9c50266d2119745216a32cc5d375607db
YapperMags <magnuspetersonmunoz@gmail.com>
2019-06-13 22:03:02 -0600

    made some changes

---
+++ backendCoding/express/advancedExpress/app.js
@@ -0,0 +1,26 @@
+var express = require("express")
+var app = express()  
+
+app.use("view engine", "ejs")
+
+app.get("/", function(req, res){
+    res.send("<h1>Hi!</h1>")
+})
+
+app.get("/fallinlovewith/:thing", function(req, res){
+ var thing = req.params.thing;
+ res.render("home", {thingVar: thing})
+})
+
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post 1", author: "Magnus"},
+        {title: "Post 2", author: "Magnus"},
+        {title: "Post 3", author: "Magnus"},
+    ]
+    res.render("posts", {posts: posts})
+})
+
+app.listen(4005, function(){
+    console.log("app.js running on PORT 4005")
+});
