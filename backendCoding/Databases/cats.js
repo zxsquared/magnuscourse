5b5c68169ce23f05f4b7898938a43b47a8ba866e
YapperMags <magnuspetersonmunoz@gmail.com>
2019-07-28 13:56:39 -0600

    made some changes

--- backendCoding/Databases/cats.js
+++ backendCoding/Databases/cats.js
@@ -8,3 +8,44 @@ var catSchema = new mongoose.Schema({
 });
 
 var Cat = mongoose.model("Cat", catSchema);
+
+// add cat to database
+
+/* var george = new Cat({
+     name: "Isaiah",
+     age: 9,
+     isCool: true
+ })
+
+ george.save(function(err, cat){
+     if(err){
+         console.log("Sothing went wrong, please try again")
+     } else{
+         console.log("We just saved a cat to our databases")
+         console.log(cat);
+     }
+ }) */
+
+Cat.create({
+    name: "Magnus",
+    age: 15,
+    isCool: false
+}, function(err, cat){
+    if(err){
+        console.log(err)
+    } else {
+        console.log(cat)
+    }
+})
+
+// retrieve cats from database
+
+Cat.find({}, function(err, cats){
+    if(err){
+        console.log("Something went wrong, please try again!")
+        console.log(err)
+    } else {
+        console.log("All the cats you wanted...")
+        console.log(cats)
+    }
+})
