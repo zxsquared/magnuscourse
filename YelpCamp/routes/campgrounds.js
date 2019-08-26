d7e8c49f12617e765a384921be79672362ce08cc
Magnus Peters Munoz <magnuspetersonmunoz@gmail.com>
2019-08-25 21:30:45 -0600

    Almosr done YelpCamp

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -17,12 +17,16 @@ router.get("/", function(req, res){
     
     })
 
-    router.post("/", function(req, res){
+    router.post("/", isLoggedIn, function(req, res){
         // get data from form
        var name = req.body.name
        var image = req.body.image
        var description = req.body.description
-       var newCampground = {name: name, image: image, description: description}
+       var author = {
+           id: req.user._id,
+           username: req.user.username
+       }
+       var newCampground = {name: name, image: image, description: description, author: author}
       
         // Create new campground and save to database
         Campground.create(newCampground, function(err, newcampground){
@@ -30,14 +34,15 @@ router.get("/", function(req, res){
                 console.log(err)
             } else {
                 // redirect back to /campgrounds page
-                res.redirect("/")
+                console.log(newCampground)
+                res.redirect("/campgrounds")
             }
         })
        
     })
 
 
-    router.get("/new", function(req, res){
+    router.get("/new", isLoggedIn, function(req, res){
         res.render("campgrounds/new")
     })
 // SHOW - Shows more info about a specific campground
@@ -53,5 +58,12 @@ router.get("/:id", function(req, res){
     })
     
 })
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect("/login")
+}
 
 module.exports = router
