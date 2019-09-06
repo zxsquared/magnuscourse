328f22583de51808129cb2ca2103a38477afdbde
Magnus Peters Munoz <magnuspetersonmunoz@gmail.com>
2019-09-06 08:04:18 -0600

    updating UI soon!

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -45,43 +45,67 @@ router.post("/", isLoggedIn, function(req, res){
     // redirect user to show page
 })
 
-// router.get("/:comment_id/edit", function(req, res){
-//    Comment.findById(req.params.comment_id, function(err, foundComment){
-//     if(err){
-//         res.redirect("back")
-//     } else {
-//         res.render("comments/edit", {campground_id: req.params.id, comment: foundComment})
-//     }
-//    }) 
+router.get("/:comment_id/edit", function(req, res){
+   Comment.findById(req.params.comment_id, function(err, foundComment){
+    if(err){
+        res.redirect("back")
+    } else {
+        res.render("comments/edit", {campground_id: req.params.id, comment: foundComment})
+    }
+   }) 
 
-// })
+})
 
-// router.put("/:comment_id", function(req, res){
-//     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
-//         if(err){
-//             res.redirect("back")
-//         } else {
-//             res.redirect("/campgrounds/" + req.params.id)
-//         }
-//     })
-// })
+router.put("/:comment_id", function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if(err){
+            res.redirect("back")
+        } else {
+            res.redirect("/campgrounds/" + req.params.id)
+        }
+    })
+})
 
-// // Comment Destroy Route
-// router.delete("/:comment_id", function(req, res){
-//     Campground.findByIdAndRemove(req.params.comment_id, function(err){
-//         if(err){
-//             res.redirect("back")
-//         } else {
-//             res.redirect("/campgrounds/" + req.params.id)
-//         }
-//     })
-// })
+// Comment Destroy Route
+router.delete("/:comment_id", function(req, res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            console.log(err)
+            res.redirect("back")
+        } else {
+            res.redirect("/campgrounds/" + req.params.id)
+        }
+    })
+})
+
+// middlewares
 
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
-        next()
+       return next()
     }
     res.redirect("/campgrounds/" + req.params.id)
+}
+
+function checkCommentOwnership(req, res, next){
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect("back")
+             
+            } else {
+                // does user own comment?
+                  if(foundComment.author.id.equals(req.user._id)){
+                next()
+                 } else {
+                    res.redirect("back")
+                 }
+                
+            }
+        })
+    } else {
+       res.redirect("back")
+    }
 }
 
 module.exports = router
